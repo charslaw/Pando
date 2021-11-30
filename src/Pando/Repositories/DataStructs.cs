14743d1c4a9318d0c8a0523142fd9386b00c35fd
Cole Schultz <schultzcole@gmail.com>
2021-11-30 04:20:06 +0000

    Use record structs for Pando\Repositories\DataStructs

--- src/Pando/Repositories/DataStructs.cs
+++ src/Pando/Repositories/DataStructs.cs
@@ -1,58 +1,7 @@
 namespace Pando.Repositories;
 
-public readonly struct SnapshotEntry
-{
-	public readonly ulong Hash;
-	public readonly ulong ParentHash;
-	public readonly ulong RootNodeHash;
+public readonly record struct SnapshotEntry(ulong Hash, ulong ParentHash, ulong RootNodeHash);
 
-	public SnapshotEntry(ulong hash, ulong parentHash, ulong rootNodeHash)
-	{
-		Hash = hash;
-		ParentHash = parentHash;
-		RootNodeHash = rootNodeHash;
-	}
+internal readonly record struct SnapshotData(ulong ParentHash, ulong RootNodeHash);
 
-	public void Deconstruct(out ulong hash, out ulong parentHash, out ulong rootNodeHash)
-	{
-		hash = Hash;
-		parentHash = ParentHash;
-		rootNodeHash = RootNodeHash;
-	}
-}
-
-internal readonly struct SnapshotData
-{
-	public readonly ulong ParentHash;
-	public readonly ulong RootNodeHash;
-
-	public SnapshotData(ulong parentHash, ulong rootNodeHash)
-	{
-		ParentHash = parentHash;
-		RootNodeHash = rootNodeHash;
-	}
-
-	public void Deconstruct(out ulong parentHash, out ulong rootNodeHash)
-	{
-		parentHash = ParentHash;
-		rootNodeHash = RootNodeHash;
-	}
-}
-
-internal readonly struct DataSlice
-{
-	public readonly int Start;
-	public readonly int Length;
-
-	public DataSlice(int start, int length)
-	{
-		Start = start;
-		Length = length;
-	}
-
-	public void Deconstruct(out int start, out int length)
-	{
-		start = Start;
-		length = Length;
-	}
-}
+internal readonly record struct DataSlice(int Start, int Length);
