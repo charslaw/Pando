1806b475eb66c016899974345a287e1aabc985cc
Cole Schultz <schultzcole@gmail.com>
2021-12-07 21:33:06 -0600

    TOOL-8 Reorganize PersistenceBackedRepository tests

--- tests/PandoTests/Repositories/PersistenceBackedRepositoryTests.cs
+++ tests/PandoTests/Repositories/PersistenceBackedRepositoryTests.cs
@@ -8,54 +8,60 @@ namespace PandoTests.Repositories;
 
 public class PersistenceBackedRepositoryTests
 {
-	[Fact]
-	public void Should_not_output_duplicate_node_to_stream_repository()
+	public class AddNode
 	{
-		// Test Data
-		var nodeData = new byte[] { 1, 2, 3, 4 };
-		var expectedIndexLength = sizeof(ulong) + sizeof(int) + sizeof(int);
-		var expectedDataLength = nodeData.Length;
+		[Fact]
+		public void Should_not_output_duplicate_node_to_stream_repository()
+		{
+			// Test Data
+			var nodeData = new byte[] { 1, 2, 3, 4 };
+			var expectedIndexLength = sizeof(ulong) + sizeof(int) + sizeof(int);
+			var expectedDataLength = nodeData.Length;
 
-		// Arrange
-		var nodeIndexStream = new MemoryStream();
-		var nodeDataStream = new MemoryStream();
-		var streamRepo = new StreamRepository(Stream.Null, nodeIndexStream, nodeDataStream);
-		var inMemoryRepo = new InMemoryRepository();
-		var repository = new PersistenceBackedRepository(inMemoryRepo, streamRepo);
+			// Arrange
+			var nodeIndexStream = new MemoryStream();
+			var nodeDataStream = new MemoryStream();
+			var streamRepo = new StreamRepository(Stream.Null, nodeIndexStream, nodeDataStream);
+			var inMemoryRepo = new InMemoryRepository();
+			var repository = new PersistenceBackedRepository(inMemoryRepo, streamRepo);
 
-		// Act
-		repository.AddNode(nodeData.CreateCopy());
-		repository.AddNode(nodeData.CreateCopy());
+			// Act
+			repository.AddNode(nodeData.CreateCopy());
+			repository.AddNode(nodeData.CreateCopy());
 
-		// Assert
-		var actualIndex = nodeIndexStream.ToArray();
-		var actualData = nodeDataStream.ToArray();
+			// Assert
+			var actualIndex = nodeIndexStream.ToArray();
+			var actualData = nodeDataStream.ToArray();
 
-		actualIndex.Length.Should().Be(expectedIndexLength);
-		actualData.Length.Should().Be(expectedDataLength);
+			actualIndex.Length.Should().Be(expectedIndexLength);
+			actualData.Length.Should().Be(expectedDataLength);
+		}
 	}
 
-	[Fact]
-	public void Should_not_output_duplicate_snapshot_to_stream_repository()
+	public class AddSnapshot
 	{
-		// Test Data
-		var parentHash = 5UL;
-		var rootNodeHash = 27UL;
-		var expectedIndexLength = sizeof(ulong) * 3;
+		[Fact]
+		public void Should_not_output_duplicate_snapshot_to_stream_repository()
+		{
+			// Test Data
+			var parentHash = 5UL;
+			var rootNodeHash = 27UL;
+			var expectedIndexLength = sizeof(ulong) * 3;
 
-		// Arrange
-		var snapshotIndexStream = new MemoryStream();
-		var streamRepo = new StreamRepository(snapshotIndexStream, Stream.Null, Stream.Null);
-		var inMemoryRepo = new InMemoryRepository();
-		var repository = new PersistenceBackedRepository(inMemoryRepo, streamRepo);
+			// Arrange
+			var snapshotIndexStream = new MemoryStream();
+			var streamRepo = new StreamRepository(snapshotIndexStream, Stream.Null, Stream.Null);
+			var inMemoryRepo = new InMemoryRepository();
+			var repository = new PersistenceBackedRepository(inMemoryRepo, streamRepo);
 
-		// Act
-		repository.AddSnapshot(parentHash, rootNodeHash);
-		repository.AddSnapshot(parentHash, rootNodeHash);
+			// Act
+			repository.AddSnapshot(parentHash, rootNodeHash);
+			repository.AddSnapshot(parentHash, rootNodeHash);
 
-		// Assert
-		var actualIndex = snapshotIndexStream.ToArray();
+			// Assert
+			var actualIndex = snapshotIndexStream.ToArray();
 
-		actualIndex.Length.Should().Be(expectedIndexLength);
+			actualIndex.Length.Should().Be(expectedIndexLength);
+		}
 	}
 }
