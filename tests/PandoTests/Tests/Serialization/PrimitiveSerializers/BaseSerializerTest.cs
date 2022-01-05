4997cda06bc552bd82e0798abe5ddca1bb3a76df
Cole Schultz <schultzcole@gmail.com>
2022-01-04 22:00:38 -0600

    PANDO-39: Add test for `ByteCount` to BaseSerializerTest

--- tests/PandoTests/Tests/Serialization/PrimitiveSerializers/BaseSerializerTest.cs
+++ tests/PandoTests/Tests/Serialization/PrimitiveSerializers/BaseSerializerTest.cs
@@ -9,12 +9,9 @@ public abstract class BaseSerializerTest<T>
 {
 	protected abstract IPrimitiveSerializer<T> Serializer { get; }
 
-	private TheoryData<T, byte[]> TestData() =>
-		throw new NotImplementedException("This is just a placeholder for the test data source that will be implemented by descendants");
-
 	[Theory]
-	[MemberData(nameof(TestData))]
-	public virtual void Should_produce_correct_bytes(T value, byte[] bytes)
+	[MemberData(nameof(TestDataProviderPlaceholders.SerializationTestData))]
+	public virtual void Serialize_should_produce_correct_bytes(T value, byte[] bytes)
 	{
 		Span<byte> nodeBytes = stackalloc byte[bytes.Length];
 		var writeBuffer = nodeBytes;
@@ -25,11 +22,27 @@ public abstract class BaseSerializerTest<T>
 	}
 
 	[Theory]
-	[MemberData(nameof(TestData))]
-	public virtual void Should_produce_correct_value(T value, byte[] bytes)
+	[MemberData(nameof(TestDataProviderPlaceholders.SerializationTestData))]
+	public virtual void Deserialize_should_produce_correct_value(T value, byte[] bytes)
 	{
 		var bytesSpan = new ReadOnlySpan<byte>(bytes);
 		var deserializationResult = Serializer.Deserialize(ref bytesSpan);
 		deserializationResult.Should().BeEquivalentTo(value);
 	}
+
+	[Theory]
+	[MemberData(nameof(TestDataProviderPlaceholders.ByteCountTestData))]
+	public virtual void ByteCount_should_return_correct_size(int? expectedSize)
+	{
+		Serializer.ByteCount.Should().Be(expectedSize);
+	}
+}
+
+internal static class TestDataProviderPlaceholders
+{
+	public static void SerializationTestData() =>
+		throw new NotImplementedException("This is just a placeholder for the test data source that will be implemented by descendants");
+
+	public static void ByteCountTestData() =>
+		throw new NotImplementedException("This is just a placeholder for the test data source that will be implemented by descendants");
 }
--- tests/PandoTests/Tests/Serialization/PrimitiveSerializers/EnumSerializerTest.cs
+++ tests/PandoTests/Tests/Serialization/PrimitiveSerializers/EnumSerializerTest.cs
@@ -15,103 +15,119 @@ public class EnumSerializerTests
 	{
 		protected override IPrimitiveSerializer<SByteEnum> Serializer => EnumSerializer.SerializerFor<SByteEnum>();
 
-		public static TheoryData<SByteEnum, byte[]> TestData => new()
+		public static TheoryData<SByteEnum, byte[]> SerializationTestData => new()
 		{
 			{ SByteEnum.Min, new byte[] { 0x80 } },
 			{ SByteEnum.Mid1, new byte[] { 0xC8 } },
 			{ SByteEnum.Mid2, new byte[] { 0x5B } },
 			{ SByteEnum.Max, new byte[] { 0x7F } },
 		};
+
+		public static TheoryData<int> ByteCountTestData => new() { sizeof(sbyte) };
 	}
 
 	public class ByteEnumSerializerTests : BaseSerializerTest<ByteEnum>
 	{
 		protected override IPrimitiveSerializer<ByteEnum> Serializer => EnumSerializer.SerializerFor<ByteEnum>();
 
-		public static TheoryData<ByteEnum, byte[]> TestData => new()
+		public static TheoryData<ByteEnum, byte[]> SerializationTestData => new()
 		{
 			{ ByteEnum.Min, new byte[] { 0x00 } },
 			{ ByteEnum.Mid1, new byte[] { 0x25 } },
 			{ ByteEnum.Mid2, new byte[] { 0xB8 } },
 			{ ByteEnum.Max, new byte[] { 0xFF } },
 		};
+
+		public static TheoryData<int> ByteCountTestData => new() { sizeof(byte) };
 	}
 
 	public class Int16EnumSerializerTests : BaseSerializerTest<Int16Enum>
 	{
 		protected override IPrimitiveSerializer<Int16Enum> Serializer => EnumSerializer.SerializerFor<Int16Enum>();
 
-		public static TheoryData<Int16Enum, byte[]> TestData => new()
+		public static TheoryData<Int16Enum, byte[]> SerializationTestData => new()
 		{
 			{ Int16Enum.Min, new byte[] { 0x00, 0x80 } },
 			{ Int16Enum.Mid1, new byte[] { 0x1F, 0xC8 } },
 			{ Int16Enum.Mid2, new byte[] { 0xC0, 0x5B } },
 			{ Int16Enum.Max, new byte[] { 0xFF, 0x7F } },
 		};
+
+		public static TheoryData<int> ByteCountTestData => new() { sizeof(short) };
 	}
 
 	public class UInt16EnumSerializerTests : BaseSerializerTest<UInt16Enum>
 	{
 		protected override IPrimitiveSerializer<UInt16Enum> Serializer => EnumSerializer.SerializerFor<UInt16Enum>();
 
-		public static TheoryData<UInt16Enum, byte[]> TestData => new()
+		public static TheoryData<UInt16Enum, byte[]> SerializationTestData => new()
 		{
 			{ UInt16Enum.Min, new byte[] { 0x00, 0x00 } },
 			{ UInt16Enum.Mid1, new byte[] { 0x40, 0x24 } },
 			{ UInt16Enum.Mid2, new byte[] { 0xE1, 0xB7 } },
 			{ UInt16Enum.Max, new byte[] { 0xFF, 0xFF } },
 		};
+
+		public static TheoryData<int> ByteCountTestData => new() { sizeof(ushort) };
 	}
 
 	public class Int32EnumSerializerTests : BaseSerializerTest<Int32Enum>
 	{
 		protected override IPrimitiveSerializer<Int32Enum> Serializer => EnumSerializer.SerializerFor<Int32Enum>();
 
-		public static TheoryData<Int32Enum, byte[]> TestData => new()
+		public static TheoryData<Int32Enum, byte[]> SerializationTestData => new()
 		{
 			{ Int32Enum.Min, new byte[] { 0x00, 0x00, 0x00, 0x80 } },
 			{ Int32Enum.Mid1, new byte[] { 0x9E, 0xAE, 0x1E, 0xC8 } },
 			{ Int32Enum.Mid2, new byte[] { 0x77, 0x95, 0xC0, 0x5B } },
 			{ Int32Enum.Max, new byte[] { 0xFF, 0xFF, 0xFF, 0x7F } },
 		};
+
+		public static TheoryData<int> ByteCountTestData => new() { sizeof(int) };
 	}
 
 	public class UInt32EnumSerializerTests : BaseSerializerTest<UInt32Enum>
 	{
 		protected override IPrimitiveSerializer<UInt32Enum> Serializer => EnumSerializer.SerializerFor<UInt32Enum>();
 
-		public static TheoryData<UInt32Enum, byte[]> TestData => new()
+		public static TheoryData<UInt32Enum, byte[]> SerializationTestData => new()
 		{
 			{ UInt32Enum.Min, new byte[] { 0x00, 0x00, 0x00, 0x00 } },
 			{ UInt32Enum.Mid1, new byte[] { 0x89, 0x6A, 0x3F, 0x24 } },
 			{ UInt32Enum.Mid2, new byte[] { 0x62, 0x51, 0xE1, 0xB7 } },
 			{ UInt32Enum.Max, new byte[] { 0xFF, 0xFF, 0xFF, 0xFF } },
 		};
+
+		public static TheoryData<int> ByteCountTestData => new() { sizeof(uint) };
 	}
 
 	public class Int64EnumSerializerTests : BaseSerializerTest<Int64Enum>
 	{
 		protected override IPrimitiveSerializer<Int64Enum> Serializer => EnumSerializer.SerializerFor<Int64Enum>();
 
-		public static TheoryData<Int64Enum, byte[]> TestData => new()
+		public static TheoryData<Int64Enum, byte[]> SerializationTestData => new()
 		{
 			{ Int64Enum.Min, new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80 } },
 			{ Int64Enum.Mid1, new byte[] { 0x00, 0xDC, 0x12, 0x75, 0x9D, 0xAE, 0x1E, 0xC8 } },
 			{ Int64Enum.Mid2, new byte[] { 0x00, 0xF8, 0x5C, 0x7A, 0x77, 0x95, 0xC0, 0x5B } },
 			{ Int64Enum.Max, new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F } },
 		};
+
+		public static TheoryData<int> ByteCountTestData => new() { sizeof(long) };
 	}
 
 	public class UInt64EnumSerializerTests : BaseSerializerTest<UInt64Enum>
 	{
 		protected override IPrimitiveSerializer<UInt64Enum> Serializer => EnumSerializer.SerializerFor<UInt64Enum>();
 
-		public static TheoryData<UInt64Enum, byte[]> TestData => new()
+		public static TheoryData<UInt64Enum, byte[]> SerializationTestData => new()
 		{
 			{ UInt64Enum.Min, new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } },
 			{ UInt64Enum.Mid1, new byte[] { 0x00, 0xFE, 0xA2, 0x85, 0x88, 0x6A, 0x3F, 0x24 } },
 			{ UInt64Enum.Mid2, new byte[] { 0x00, 0x18, 0xED, 0x8A, 0x62, 0x51, 0xE1, 0xB7 } },
 			{ UInt64Enum.Max, new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF } },
 		};
+
+		public static TheoryData<int> ByteCountTestData => new() { sizeof(ulong) };
 	}
 }
