e15932380b77d9f2daa1c7a6752aaf7779cf3982
Cole Schultz <schultzcole@gmail.com>
2022-01-09 10:14:35 -0600

    PANDO-38: integral numeric serializers use FixedSizeBaseSerializer
    This *greatly* reduces the amount of code in these serializers, meaning less chance of bugs due to missing or mis-implemented handling of error cases and post conditions

--- src/Pando/Serialization/PrimitiveSerializers/UInt64LittleEndianSerializer.cs
+++ src/Pando/Serialization/PrimitiveSerializers/UInt64LittleEndianSerializer.cs
@@ -1,38 +1,15 @@
 using System;
 using System.Buffers.Binary;
-using System.Runtime.CompilerServices;
-using Pando.Serialization.Utils;
 
 namespace Pando.Serialization.PrimitiveSerializers;
 
 /// Serializes/deserializes <c>ulong</c> values in little endian encoding (least significant byte first)
-public class UInt64LittleEndianSerializer : IPrimitiveSerializer<ulong>
+public class UInt64LittleEndianSerializer : FixedSizeBaseSerializer<ulong>
 {
 	/// <summary>A global default instance for <see cref="UInt64LittleEndianSerializer"/></summary>
 	public static UInt64LittleEndianSerializer Default { get; } = new();
 
-	private const int SIZE = sizeof(ulong);
-
-	public int? ByteCount
-	{
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		get => SIZE;
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public int ByteCountForValue(ulong value) => SIZE;
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public void Serialize(ulong value, ref Span<byte> buffer)
-	{
-		var slice = SpanHelpers.PopStart(ref buffer, SIZE);
-		BinaryPrimitives.WriteUInt64LittleEndian(slice, value);
-	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public ulong Deserialize(ref ReadOnlySpan<byte> buffer)
-	{
-		var slice = SpanHelpers.PopStart(ref buffer, SIZE);
-		return BinaryPrimitives.ReadUInt64LittleEndian(slice);
-	}
+	protected override int FixedSize => sizeof(ulong);
+	protected override void SerializeInner(ulong value, Span<byte> slice) => BinaryPrimitives.WriteUInt64LittleEndian(slice, value);
+	protected override ulong DeserializeInner(ReadOnlySpan<byte> slice) => BinaryPrimitives.ReadUInt64LittleEndian(slice);
 }
