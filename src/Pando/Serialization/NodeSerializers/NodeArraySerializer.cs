1c5ea63a18b3f1907b168006ae8866cf97533775
Cole Schultz <schultzcole@gmail.com>
2022-01-22 15:09:53 -0600

    PANDO-52: Rename ArrayNodeListBuilder -> ArrayBuilder
    And also remove self-explanatory/useless comment

--- src/Pando/Serialization/NodeSerializers/NodeArraySerializer.cs
+++ src/Pando/Serialization/NodeSerializers/NodeArraySerializer.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 namespace Pando.Serialization.NodeSerializers;
 
 /// Serializes a node that is an array of nodes using the given node serializer.
-public class NodeArraySerializer<T> : BaseNodeListSerializer<T[], T, NodeArraySerializer<T>.ArrayNodeListBuilder>
+public class NodeArraySerializer<T> : BaseNodeListSerializer<T[], T, NodeArraySerializer<T>.ArrayBuilder>
 {
 	public NodeArraySerializer(INodeSerializer<T> elementSerializer) : base(elementSerializer) { }
 
@@ -14,17 +14,15 @@ public class NodeArraySerializer<T> : BaseNodeListSerializer<T[], T, NodeArraySe
 	protected override T ListGetElement(T[] array, int index) => array[index];
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	protected override ArrayNodeListBuilder CreateListBuilder(int size) => new(size);
+	protected override ArrayBuilder CreateListBuilder(int size) => new(size);
 
-	/// INodeListBuilder implementation for a basic array.
-	/// The _array instance here is intended to live beyond the lifetime of the ArrayNodeListBuilder.
-	/// Reusing an ArrayNodeListBuilder is not safe because the internal _array is probably being used elsewhere.
-	public struct ArrayNodeListBuilder : INodeListBuilder
+	// struct to avoid allocations beyond the actual array instance
+	public struct ArrayBuilder : INodeListBuilder
 	{
 		private readonly T[] _array;
 		private int _count;
 
-		public ArrayNodeListBuilder(int capacity)
+		public ArrayBuilder(int capacity)
 		{
 			_count = 0;
 			_array = new T[capacity];
