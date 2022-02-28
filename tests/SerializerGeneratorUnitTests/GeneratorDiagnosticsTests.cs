8a903d112aa1c950dd44c8cf1c9d5425d8d84cd0
Cole Schultz <schultzcole@gmail.com>
2022-02-27 21:54:52 -0600

    Use *nix style directory separators

--- tests/SerializerGeneratorUnitTests/GeneratorDiagnosticsTests.cs
+++ tests/SerializerGeneratorUnitTests/GeneratorDiagnosticsTests.cs
@@ -11,13 +11,13 @@ namespace SerializerGeneratorUnitTests;
 public class GeneratorDiagnosticsTests
 {
 	[Theory]
-	[InlineData(@"TestClasses\InvalidBecauseAbstract.cs", "Pando_SG_0001")]
-	[InlineData(@"TestClasses\InvalidBecauseNotSealed.cs", "Pando_SG_0001")]
-	[InlineData(@"TestClasses\InvalidBecauseNoDeconstruct.cs", "Pando_SG_0002")]
-	[InlineData(@"TestClasses\InvalidBecauseNoDeconstructStruct.cs", "Pando_SG_0002")]
-	[InlineData(@"TestClasses\InvalidBecauseNoMatchingConstructor.cs", "Pando_SG_0003")]
-	[InlineData(@"TestClasses\InvalidBecauseNoMatchingConstructorStruct.cs", "Pando_SG_0003")]
-	[InlineData(@"TestClasses\InvalidBecauseMultipleMatchingConstructor.cs", "Pando_SG_0004")]
+	[InlineData("TestClasses/InvalidBecauseAbstract.cs", "Pando_SG_0001")]
+	[InlineData("TestClasses/InvalidBecauseNotSealed.cs", "Pando_SG_0001")]
+	[InlineData("TestClasses/InvalidBecauseNoDeconstruct.cs", "Pando_SG_0002")]
+	[InlineData("TestClasses/InvalidBecauseNoDeconstructStruct.cs", "Pando_SG_0002")]
+	[InlineData("TestClasses/InvalidBecauseNoMatchingConstructor.cs", "Pando_SG_0003")]
+	[InlineData("TestClasses/InvalidBecauseNoMatchingConstructorStruct.cs", "Pando_SG_0003")]
+	[InlineData("TestClasses/InvalidBecauseMultipleMatchingConstructor.cs", "Pando_SG_0004")]
 	public void Should_produce_correct_diagnostic_for_invalid_cases(string sourceFile, string expectedDiagnosticId)
 	{
 		var baseCompilation = CompilationUtils.CreateCompilationWithFiles(sourceFile);
