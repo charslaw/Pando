caceb96c23f1c34ec972a063ae7010375a60c2b5
Cole <schultzcole@gmail.com>
2025-02-16 18:30:07 -0600

    Remove Pando Source Generator

--- src/Pando.SerializerGenerator/Utils/IsExternalInit.cs
+++
@@ -1,7 +0,0 @@
-// Having this class present in the compilation allows for init setters in properties
-// ReSharper disable UnusedType.Global
-// ReSharper disable once CheckNamespace
-
-namespace System.Runtime.CompilerServices;
-
-internal class IsExternalInit { }
--- tests/SerializerGeneratorUnitTests/TestClasses/ValidRecordStruct.cs
+++
@@ -1,6 +0,0 @@
-using Pando.SerializerGenerator.Attributes;
-
-namespace SerializerGeneratorUnitTests.TestClasses;
-
-[GenerateNodeSerializer]
-internal record struct ValidRecordStruct(int Prop1, int Prop2);
