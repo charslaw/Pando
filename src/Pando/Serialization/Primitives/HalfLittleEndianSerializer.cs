c787c3d2320f499109a598e454a9300babb1ad5e
Cole <schultzcole@gmail.com>
2025-03-02 13:02:21 -0600

    disallow unsafe blocks in Pando

--- src/Pando/Serialization/Primitives/HalfLittleEndianSerializer.cs
+++ src/Pando/Serialization/Primitives/HalfLittleEndianSerializer.cs
@@ -9,7 +9,7 @@ public class HalfLittleEndianSerializer : IPandoSerializer<Half>
 	/// <summary>A global default instance for <see cref="SingleLittleEndianSerializer"/></summary>
 	public static HalfLittleEndianSerializer Default { get; } = new();
 
-	public unsafe int SerializedSize => sizeof(Half);
+	public int SerializedSize => sizeof(short);
 
 	public void Serialize(Half value, Span<byte> buffer, INodeDataStore _) => BinaryPrimitives.WriteHalfLittleEndian(buffer, value);
 
