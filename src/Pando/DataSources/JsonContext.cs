ad9a068a51000280030983319c6f7d7016888446
Cole <schultzcole@gmail.com>
2025-03-02 22:29:35 -0600

    Add JsonNodeStore
    This is primarily a debugging tool to make it easier to visualize the organization of data in the node store

---
+++ src/Pando/DataSources/JsonContext.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Pando.Repositories;
+
+namespace Pando.DataSources;
+
+[JsonSourceGenerationOptions(
+	Converters = [typeof(NodeIdConverter), typeof(ByteArrayJsonArrayConverter)],
+	WriteIndented = true
+)]
+[JsonSerializable(typeof(Dictionary<NodeId, byte[]>))]
+internal partial class JsonContext : JsonSerializerContext;
+
+internal class NodeIdConverter : JsonConverter<NodeId>
+{
+	public override NodeId ReadAsPropertyName(
+		ref Utf8JsonReader reader,
+		Type typeToConvert,
+		JsonSerializerOptions options
+	)
+	{
+		return Read(ref reader, typeToConvert, options);
+	}
+
+	public override void WriteAsPropertyName(Utf8JsonWriter writer, NodeId value, JsonSerializerOptions options)
+	{
+		Span<char> buffer = stackalloc char[NodeId.SIZE * 2]; // hex representation is 2 chars per byte
+		value.CopyHashStringTo(ref buffer);
+		writer.WritePropertyName(buffer);
+	}
+
+	public override NodeId Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		Span<char> buffer = stackalloc char[NodeId.SIZE * 2]; // hex representation is 2 chars per byte
+		reader.CopyString(buffer);
+		return NodeId.FromHashString(buffer);
+	}
+
+	public override void Write(Utf8JsonWriter writer, NodeId value, JsonSerializerOptions options)
+	{
+		Span<char> buffer = stackalloc char[NodeId.SIZE * 2]; // hex representation is 2 chars per byte
+		value.CopyHashStringTo(ref buffer);
+		writer.WriteStringValue(buffer);
+	}
+}
+
+internal class ByteArrayJsonArrayConverter : JsonConverter<byte[]>
+{
+	public override byte[] Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		var str = reader.GetString()!;
+		return Convert.FromHexString(str);
+	}
+
+	public override void Write(Utf8JsonWriter writer, byte[] value, JsonSerializerOptions options)
+	{
+		Span<char> buffer = stackalloc char[value.Length * 2];
+		if (Convert.TryToHexString(value, buffer, out var charsWritten))
+		{
+			buffer = buffer[..charsWritten];
+		}
+		else
+		{
+			throw new InvalidOperationException(
+				"Allocated buffer was not big enough for hex string representation of byte array."
+			);
+		}
+		writer.WriteStringValue(buffer);
+	}
+}
