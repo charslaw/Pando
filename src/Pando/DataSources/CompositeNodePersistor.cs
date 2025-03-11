f7b04697561e2b0100719bc4fbd5c75a013c0837
Cole <schultzcole@gmail.com>
2025-03-10 23:35:38 -0500

    Add CompositeNodePersistor

---
+++ src/Pando/DataSources/CompositeNodePersistor.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Pando.Repositories;
+
+namespace Pando.DataSources;
+
+public class CompositeNodePersistor : INodePersistor
+{
+	private readonly INodePersistor[] _persistors;
+	private readonly INodePersistor _primaryPersistor;
+
+	public CompositeNodePersistor(IEnumerable<INodePersistor> persistors)
+	{
+		_persistors = persistors.ToArray();
+		_primaryPersistor = _persistors[0];
+	}
+
+	public void PersistNode(NodeId nodeId, ReadOnlySpan<byte> data)
+	{
+		foreach (var persistor in _persistors)
+		{
+			persistor.PersistNode(nodeId, data);
+		}
+	}
+
+	public (Dictionary<NodeId, Range>, byte[]) LoadNodeData()
+	{
+		return _primaryPersistor.LoadNodeData();
+	}
+}
